2e082a3eeb37a34d3ab6ec06678e60c8b4f20099
wangning <1132258441@qq.com>
2015-06-05 14:02:03 +0800

    Initial Commit

---
+++ FirstSwitf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstSwitf
+//
+//  Created by chyjoyboy on 15/6/5.
+//  Copyright (c) 2015年 zxjr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
